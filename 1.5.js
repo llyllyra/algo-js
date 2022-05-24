be03437f25dfc4c6c2ebad2a279f68785b11bf9f
Llyllyra <llyllyrallylly@gmail.com>
2022-05-24 11:43:37 +0200

    Create 1.5.js

---
+++ 1.5.js
@@ -0,0 +1,12 @@
+// Ask two numbers with decimal part to the user. For the first one only keep the integer part. Then multiply them and display the result.
+const readlineSync = require("readline-sync");
+//demandez les nombres
+
+let nomberOne = Number(readlineSync.question("Quelle est votre premier chiffre a virgule"))
+let nomberTwo= Number(readlineSync.question("Quelle est votre deuxieme chiffre a virgule"))
+//faire le calcul
+let nomber = Number.parseInt(nomberOne) * nomberTwo
+
+
+//Afficher le chiffre
+console.log(nomber)
