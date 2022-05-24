3e4b606846f347ee8450ae6caad5b6928b8d7b45
Llyllyra <llyllyrallylly@gmail.com>
2022-05-24 10:56:58 +0200

    1.2 + création 1.3

---
+++ 1.2.js
@@ -0,0 +1,11 @@
+// Define a variable and display it in the console.
+
+//Definir 3 variables
+
+let name = "Bouvard";
+let firstName = "Marlene";
+let city = "Namur";
+
+//afficher les variables
+
+console.log(`Your name is ${firstName} ${name} and you live in ${city}`)
