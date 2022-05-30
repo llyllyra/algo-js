c9fa58bdb91a69261ce938f9d2a3d68020ff2986
Llyllyra <llyllyrallylly@gmail.com>
2022-05-30 12:23:42 +0200

    update

--- 03/3.2.js
+++ 03/3.2.js
@@ -1,9 +1,8 @@
 //Write a program that will calculate the average value of a given array.
-let number = [100, 101, 102]
-let summ = number.reduce(
-    (a, b) => a + b, 0
-);
+let number = [100, 101, 102];
+let sum = number.reduce((a, b) => a + b, 0);
 
-
-console.log(summ/number.length)
+let calc = sum/number.length;
+console.log(calc);
+console.log(typeof sum)
 
--- 03/3.3.js
+++ 03/3.3.js
@@ -1,16 +1,7 @@
 // Write a program that will create a duplicate of a given array.
 
 numbers = [1, 2, 3];
-numbersCopy = [];
-numbers.forEach((number) => {
-    numbersCopy.push(number)
-})
-/*
-for (let elem of numbers)
-{
-    numbersCopy.push(elem)
-}
+numbersCopy = [...numbers];
 
- */
 console.log(numbersCopy)
 
