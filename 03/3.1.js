1227c67048eb42f5a41d7e161ab59b231c4cc648
Llyllyra <llyllyrallylly@gmail.com>
2022-05-30 11:53:48 +0200

    Update 3.1.js

--- 03/3.1.js
+++ 03/3.1.js
@@ -1,17 +1,16 @@
 // Write a program that will add all the elements of an array.
 
 let number =  [1,2,3,4,5]
-/*
-let somme = 0
+let number2 = [100, 101, 102]
 
-for (let elem of number){
-    somme += elem
-}
-*/
+let sum = number.reduce(
+    (a, b) => a + b, 0
+);
 
-let somme = number.reduce(
+let sum1 = number2.reduce(
     (a, b) => a + b, 0
 );
 
 
-console.log(somme)
+console.log(sum)
+console.log(sum1)
