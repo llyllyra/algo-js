9114b14b558a28cf5b4422edf5f43ec48a1b17b8
Llyllyra <llyllyrallylly@gmail.com>
2022-05-30 13:59:47 +0200

    correction

--- 03/3.4.js
+++ 03/3.4.js
@@ -1,6 +1,6 @@
 // Write a program that will display the minimum and maximum elements of a given array.
 
-let array = [1, 2, 10, 4, 5]
+let array = [1, 2, 10, 4, 5];
 
-console.log(Math.min(...array))
-console.log(Math.max(...array))
+console.log(Math.min(...array));
+console.log(Math.max(...array));
--- 03/3.5.js
+++ 03/3.5.js
@@ -1,24 +1,19 @@
-// You get an array of numbers, return the sum of all of the positives ones.
-
 //You get an array of numbers, return the sum of all of the positives ones.
 //
 // Example: [1,-4,7,12] => 1 + 7 + 12 = 20
 //
 // Note: if there is nothing to sum, the sum is default to 0
 
-let array = [,-4,7,12]
-let sum = 0
+let array = [1, -4, 7, 12]
+let sum
 
-if(array.length === 0)
-{
-    sum = 0
-}
 
-for (let elem of array)
-{
-    if(elem >= 0){
-        sum += elem
-    }
-}
-console.log(sum)
+const pos = array.filter((el) => el >= 0)
+                .reduce((previous, current) => previous + current, 0);
+
+
+// correction nico
+
+
+console.log(pos);
 
